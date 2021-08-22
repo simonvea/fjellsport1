25087e93e8be2e8d3ffd55fc87a4fecb98fd59da
Simon Opheim <simon.opheim@gmail.com>
2021-08-22 21:30:11 +0200

    save filter history

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ import {
 
 const app = document.getElementById('app');
 const filter = document.querySelector('.filter');
+const filterAutocompleteElement = document.getElementById('history');
 
 const orders = [];
 
@@ -29,13 +30,37 @@ try {
 }
 
 const reRenderOrdersWithFilter = debounce((e) => {
-  const filteredOrders = filterOrders(e.target.value, orders);
+  const filter = e.target.value;
+  const filteredOrders = filterOrders(filter, orders);
 
   if (filteredOrders.length < 1) {
     renderNotFound(app);
   } else {
     renderOrders(app, filteredOrders);
   }
+  addFilterToHistory(filter, filterAutocompleteElement);
 }, 500);
+
+function addFilterToHistory(filter, historyElement) {
+  if (!filter) return;
+
+  const HISTORY_KEY = 'filterHistory';
+  const oldHistory = localStorage.getItem(HISTORY_KEY);
+  const newFilter = filter.toLowerCase(); // Ensure case insensitivity to have a cleaner history
+
+  const newHistory = [newFilter];
+
+  if (oldHistory) {
+    const history = JSON.parse(oldHistory);
+    if (!history.includes(newFilter)) {
+      newHistory.push(...history);
+    }
+  }
+
+  historyElement.innerHTML = newHistory
+    .map((f) => `<option value="${f}"></option>`)
+    .join('');
+  localStorage.setItem(HISTORY_KEY, JSON.stringify(newHistory));
+}
 
 filter.addEventListener('input', reRenderOrdersWithFilter);
