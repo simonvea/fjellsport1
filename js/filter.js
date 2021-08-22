65a32df2977e8b3679d83ea78ba1243e840df9c7
Simon Opheim <simon.opheim@gmail.com>
2021-08-22 20:51:05 +0200

    add orderId and productId to filter

--- js/filter.js
+++ js/filter.js
@@ -1,15 +1,30 @@
 export function filterOrders(filter, orders) {
   if (!filter) return orders;
 
-  return orders.filter((order) => itemsHasMatch(order.lineItems, filter));
+  return orders.filter(
+    (order) =>
+      itemsHasMatch(order.lineItems, filter) || orderHasMatch(order, filter)
+  );
 }
 
 function itemsHasMatch(items, toMatch) {
-  return items.some((item) => nameIncludesString(item, toMatch));
+  return items.some(
+    (item) =>
+      nameIncludesString(item, toMatch) ||
+      idIncludesString(item.productId, toMatch)
+  );
 }
 
 function nameIncludesString(item, name) {
   const regExp = new RegExp(name, 'ig');
 
   return regExp.test(item.name);
+}
+
+function idIncludesString(id, filter) {
+  return id.includes(filter);
+}
+
+function orderHasMatch(order, filter) {
+  return idIncludesString(order.orderId, filter);
 }
