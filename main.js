753b1bf8e6e59037d98fa3e2131e5a6f1e494022
Simon Opheim <simon.opheim@gmail.com>
2021-08-22 17:41:49 +0200

    move HTML to Javascript
    Handle the generation of HTML through Javascript in order
    to make rendering more dynamic.

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import 'normalize.css';
 import './style.css';
+import { orders } from './orderExamples.json';
+import { ordersToHtml } from './htmlFunctions';
+
+const app = document.getElementById('app');
+
+const html = ordersToHtml(orders);
+
+app.innerHTML = html;
